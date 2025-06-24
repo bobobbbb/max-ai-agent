b89cc360454b5d1da917d27950639892ab9f6885
bobobbbb <1665052137@qq.com>
2025-06-24 16:25:55 +0800

    feat: 初始化文件

---
+++ src/main/java/com/max/maxaiagent/MaxAiAgentApplication.java
@@ -0,0 +1,13 @@
+package com.max.maxaiagent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaxAiAgentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MaxAiAgentApplication.class, args);
+    }
+
+}
