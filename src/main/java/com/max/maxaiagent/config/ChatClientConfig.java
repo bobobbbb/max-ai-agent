e476083127a8d415bb988d12ca220ef55cb38537
bobobbbb <1665052137@qq.com>
2025-06-27 17:05:56 +0800

    feat: 新增配置类，将chatclient放入config类中，优化代码结构

---
+++ src/main/java/com/max/maxaiagent/config/ChatClientConfig.java
@@ -0,0 +1,37 @@
+package com.max.maxaiagent.config;
+
+import com.max.maxaiagent.advisor.MyLoggerAdvisor;
+import com.max.maxaiagent.memory.RedisChatMemory;
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
+import org.springframework.ai.chat.client.advisor.api.Advisor;
+import org.springframework.ai.chat.model.ChatModel;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ChatClientConfig {
+    private static final String SYSTEMPROMOTE="你是一位专门辅导面试的“计算机网络面霸型专家”，目标是帮助候选人通过技术面试。你熟知计算机网络的所有高频八股文题，了解面试官心理，知道怎样的答案最容易打动他们。\n" +
+            "\n" +
+            "对于每一个面试相关问题，请遵循以下格式回答：\n" +
+            "\n" +
+            "\uD83D\uDFE0 一句话通关总结： 用一句话提炼出核心观点，便于记忆。\n" +
+            "\n" +
+            "面试高分回答模板： 条理清晰、术语准确、语言简洁，展示候选人技术深度。\n" +
+            "\n" +
+            "面试加分技巧： 可选部分，点出候选人可以额外说什么来打动面试官，展现主动性或工程实践经验。\n" +
+            "\n" +
+            "回答风格应简洁、干脆、有逻辑感，突出“专业 + 熟练 + 思路清晰”。";
+    @Bean
+    public ChatClient dashScopeChatClient(ChatModel dashScopeChatModel,RedisChatMemory redisChatMemory){
+        return ChatClient.builder(dashScopeChatModel)
+                .defaultSystem(SYSTEMPROMOTE)
+                .defaultAdvisors(
+                        new MessageChatMemoryAdvisor(redisChatMemory),
+                        new MyLoggerAdvisor()
+                )
+                .build();
+
+    }
+
+}
--- src/main/java/com/max/maxaiagent/service/ChatClientService.java
+++ src/main/java/com/max/maxaiagent/service/ChatClientService.java
@@ -2,6 +2,7 @@ package com.max.maxaiagent.service;
 
 import com.max.maxaiagent.advisor.MyLoggerAdvisor;
 import com.max.maxaiagent.memory.RedisChatMemory;
+import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
@@ -18,38 +19,14 @@ import static org.springframework.ai.chat.client.advisor.AbstractChatMemoryAdvis
 
 @Component
 @Slf4j
+@AllArgsConstructor
 public class ChatClientService {
-    private final ChatClient chatClient;
-    //系统提示词
-    private static final String SYSTEMPROMOTE="你是一位专门辅导面试的“计算机网络面霸型专家”，目标是帮助候选人通过技术面试。你熟知计算机网络的所有高频八股文题，了解面试官心理，知道怎样的答案最容易打动他们。\n" +
-            "\n" +
-            "对于每一个面试相关问题，请遵循以下格式回答：\n" +
-            "\n" +
-            "\uD83D\uDFE0 一句话通关总结： 用一句话提炼出核心观点，便于记忆。\n" +
-            "\n" +
-            "面试高分回答模板： 条理清晰、术语准确、语言简洁，展示候选人技术深度。\n" +
-            "\n" +
-            "面试加分技巧： 可选部分，点出候选人可以额外说什么来打动面试官，展现主动性或工程实践经验。\n" +
-            "\n" +
-            "回答风格应简洁、干脆、有逻辑感，突出“专业 + 熟练 + 思路清晰”。";
+    private final ChatClient dashScopeChatClient;
     private final Advisor aliRagCloudAdvisor;
-
-    //创建一个DashScope的ChatModel
-    public ChatClientService(ChatModel dashScopeChatModel, Advisor aliRagCloudAdvisor, RedisChatMemory redisChatMemory) {
-
-        chatClient = ChatClient.builder(dashScopeChatModel)
-                .defaultSystem(SYSTEMPROMOTE)
-                .defaultAdvisors(
-                        new MessageChatMemoryAdvisor(redisChatMemory),
-                        new MyLoggerAdvisor()
-                )
-                .build();
-        this.aliRagCloudAdvisor = aliRagCloudAdvisor;
-    }
     //开始会话
     public Flux<String> doChat(String message, String chatId) {
         System.out.println("doChat called with message: " + message + ", chatId: " + chatId);
-        Flux<String> content = chatClient
+        Flux<String> content = dashScopeChatClient
                 .prompt()
                 .user(message)
                 //会话记忆advisor
@@ -59,7 +36,6 @@ public class ChatClientService {
                 .advisors(aliRagCloudAdvisor)
                 .stream()
                 .content();
-        // log。info打印内容
         content.subscribe(it -> log.info("doChat emit: " + it));
         return content;
     }
